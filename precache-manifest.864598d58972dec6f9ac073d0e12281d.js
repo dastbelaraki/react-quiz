c9db448f29b7243fccdda13f1f5c60b0707e4e78
imanDB <iman.dastbelaraki@gmail.com>
2018-10-11 10:33:53 +0330

    Updates

---
+++ precache-manifest.864598d58972dec6f9ac073d0e12281d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "63f5d214189569998a8c1e011b98bce2",
+    "url": "/react-quiz/static/media/second-choice.63f5d214.png"
+  },
+  {
+    "revision": "b36af3116c68078cfeff732567dc81c7",
+    "url": "/react-quiz/static/media/quiz-bg.b36af311.png"
+  },
+  {
+    "revision": "383c84d06eaf06e57c353cf05ab7e394",
+    "url": "/react-quiz/static/media/main-bg.383c84d0.png"
+  },
+  {
+    "revision": "a33960f8c6fe83900b66772239a1fec0",
+    "url": "/react-quiz/static/media/first-choice.a33960f8.png"
+  },
+  {
+    "revision": "af45168ecde8330867c4",
+    "url": "/react-quiz/static/js/runtime~main.af45168e.js"
+  },
+  {
+    "revision": "3f8cda6054b2b9a4d815",
+    "url": "/react-quiz/static/js/main.3f8cda60.chunk.js"
+  },
+  {
+    "revision": "edd9598a93ba31ae6e09",
+    "url": "/react-quiz/static/js/1.edd9598a.chunk.js"
+  },
+  {
+    "revision": "3f8cda6054b2b9a4d815",
+    "url": "/react-quiz/static/css/main.129b3a57.chunk.css"
+  },
+  {
+    "revision": "a78349e7c1571759d2b775982dcd3096",
+    "url": "/react-quiz/index.html"
+  }
+];
